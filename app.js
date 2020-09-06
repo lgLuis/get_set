2e659d496dd125c102fd478a83063949aa61a772
lgLuis <luis_unlz@yahoo.com.ar>
2020-09-06 13:33:54 -0300

    def inicial

---
+++ app.js
@@ -0,0 +1,5 @@
+/*
+MÉTODOS permite acceder a datos de los objetos, para leerlos o asignar nuevos valores. 
+MÉT setter lo que hace es asignar un valor.
+MÉT getter se encarga de recibir un valor. 
+*/
